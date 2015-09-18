d7e7100ab1658a72c0cdbe695399dcb52172f770
patrick.opie <Patrick.Opie@T-Mobile.com>
2015-09-18 10:11:31 -0700

    Angular2 app

---
+++ angular2/tsd_typings/tsd.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="angular2/angular2.d.ts" />
+/// <reference path="angular2/router.d.ts" />
+/// <reference path="es6-promise/es6-promise.d.ts" />
+/// <reference path="../node_modules/typescript-simple-loader/dist/typescript-simple-loader.d.ts" />
