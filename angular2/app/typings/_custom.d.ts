3232598735ff61c873dcc9cc2b020ca0465ac0fd
patrick.opie <Patrick.Opie@T-Mobile.com>
2015-09-18 10:11:31 -0700

    Angular2 app

---
+++ angular2/app/typings/_custom.d.ts
@@ -0,0 +1,10 @@
+/*
+ * Our custom types
+ */
+/// <reference path="browser.d.ts" />
+/// <reference path="ng2.d.ts" />
+
+/*
+ * tsd generated types
+ */
+/// <reference path="../../tsd_typings/tsd.d.ts" />
